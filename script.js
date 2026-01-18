e14d58e1e83e572b2362f7dadd554057ee5d6d2b
Wesleyjjx-dev <wesleydelaordellavega@hotmail.com>
2026-01-18 20:51:15 -0300

    mantendo tarefas mesmo fechado a aba

--- script.js
+++ script.js
@@ -2,18 +2,40 @@ const input = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const list = document.getElementById("taskList");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.textContent = task.text;
+
+    if (task.done) {
+      li.classList.add("done");
+    }
+
+    li.addEventListener("click", () => {
+      tasks[index].done = !tasks[index].done;
+      saveTasks();
+      renderTasks();
+    });
+
+    list.appendChild(li);
+  });
+}
+
 function addTask() {
   const text = input.value.trim();
   if (text === "") return;
 
-  const li = document.createElement("li");
-  li.textContent = text;
-
-  li.addEventListener("click", () => {
-    li.classList.toggle("done");
-  });
-
-  list.appendChild(li);
+  tasks.push({ text, done: false });
+  saveTasks();
+  renderTasks();
   input.value = "";
 }
 
@@ -24,3 +46,6 @@ input.addEventListener("keypress", (e) => {
     addTask();
   }
 });
+
+// carrega tarefas ao abrir a página
+renderTasks();
